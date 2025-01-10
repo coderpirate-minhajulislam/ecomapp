e1618f89a4f06c92bcd97ab1bdebb6042813e08e
coderpirate-minhajulislam <coderpirate.minhajulislam@gmail.com>
2025-01-11 01:49:51 +0600

    fix file permission

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,14 @@
-@php
+{{-- @php
     $role = Auth::user()->role;
-    $redirect = 'dashboard';
+    'dashboard' = 'dashboard';
     if ($role == 'admin') {
-        $redirect = 'admin.dashboard';
+        'dashboard' = 'admin.dashboard';
     } elseif ($role == 'user') {
-        $redirect = 'user.dashboard';
+        'dashboard' = 'user.dashboard';
     }else {
         $redirect = 'dashboard';
     }
-@endphp
+@endphp --}}
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -16,14 +16,14 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route($redirect) }}">
+                    <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route($redirect)" :active="request()->routeIs($redirect)">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
@@ -78,7 +78,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route($redirect)" :active="request()->routeIs($redirect)">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
