984e34fdbd2f229262e55692b75586a376518fe9
coderpirate-minhajulislam <coderpirate.minhajulislam@gmail.com>
2025-01-11 16:52:47 +0600

    admin dashboard update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
-    }// end of method
+        return view('admin.index');
+    } // end of method
+
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    } // end of method
+
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return view('admin.admin_login');
+    } // end of method
 }
